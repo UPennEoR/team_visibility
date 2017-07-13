78318bb707ae5bef14ca34683042e2a8c61f0963
Roshan Benefo <rbenefoz@gmail.com>
2017-07-13 01:42:13 -0400

    sdf

--- DelayTransformV2.py
+++ DelayTransformV2.py
@@ -130,60 +130,30 @@ def delaytransformv1(data_dir, stokes):
 	#type-abaselines = ['72_112', '97_112', '22_105', '9_88', '9_20', '20_89', '43_89', '53_64', '31_53', '31_65', '80_104', '96_104']
 	#type-cbaselines = ['72_105', '88_105', '22_112', '9_22', '9_64', '20_53', '53_80', '10_89', '31_89', '31_104', '43_65', '65_96']
 	baselines = ['64_88', '64_80', '9_105', '9_53', '53_104', '22_72', '20_22', '20_31', '31_96', '65_89', '10_97', '10_43', '72_105', '88_105', '22_112', '9_22', '9_64', '20_53', '53_80', '10_89', '31_89', '31_104', '43_65', '65_96', '72_112', '97_112', '22_105', '9_88', '9_20', '20_89', '43_89', '53_64', '31_53', '31_65', '80_104', '96_104']
-	if stokes == "I" or stokes == "Q":
-		t_xx, d_xx, f_xx = capo.miriad.read_files([xx_data[i]], antstr=antstr, polstr='xx', verbose=True)
-		t_yy, d_yy, f_yy = capo.miriad.read_files([yy_data[i]], antstr=antstr, polstr='yy', verbose=True)
-		for antstr in baselines:
-			ant_i, ant_j = map(int, antstr.split('_'))
-			pair = (ant_i, ant_j)
-			data, channels = avgfreqcalc(data_dir, antstr, stokes, d_xx, d_yy)
-			window = aipy.dsp.gen_window(channels, window="blackman-harris")
-			d_transform = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(data * window)))
-			delays = np.fft.fftshift(np.fft.fftfreq(channels, .1/channels)) # fftfreq takes in (nchan, chan_spacing)
-			d_start = delays[0]
-			d_end = delays[-1]
-			#d_transform = np.abs(d_transform)
-			f, ax = plt.subplots(figsize=(5, 4))
-			ax.plot(delays, np.log(np.abs(d_transform)))
-			tauh = calculate_baseline(pair)/2.9979e8*1e9 # convert to ns
-			ax = plt.gca()
-			ax.axvline(x=0., linestyle='--', color='0.5')
-			ax.axvline(x=-tauh, linestyle='--', color='0.5')
-			ax.axvline(x=tauh, linestyle='--', color='0.5')
-			ax.set_xlim(-400, 400)
-			ax.set_ylim(-10, -2)
-			ax.set_xlabel('Delay [bins]')
-			ax.set_ylabel('log10(abs(V_I)')
-			ax.set_title('Delay Transform'+antstr+stokes)
-			plt.savefig("/data4/paper/rkb/delaygifstorage/"+'delaytransform'+'{} {}.png'.format(antstr, stokes))
-			plt.clf()
-	if stokes =="U" or stokes =="V":
-		t_xy, d_xy, f_xy = capo.miriad.read_files([xy_data[i]], antstr=antstr, polstr='xy', verbose=True)
-		t_yx, d_yx, f_yx = capo.miriad.read_files([yx_data[i]], antstr=antstr, polstr='yx', verbose=True)
-		for antstr in baselines:
-			ant_i, ant_j = map(int, antstr.split('_'))
-			pair = (ant_i, ant_j)
-			data, channels = avgfreqcalc(data_dir, antstr, stokes, d_xy, d_yx)
-			window = aipy.dsp.gen_window(channels, window="blackman-harris")
-			d_transform = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(data * window)))
-			delays = np.fft.fftshift(np.fft.fftfreq(channels, .1/channels)) # fftfreq takes in (nchan, chan_spacing)
-			d_start = delays[0]
-			d_end = delays[-1]
-			#d_transform = np.abs(d_transform)
-			f, ax = plt.subplots(figsize=(5, 4))
-			ax.plot(delays, np.log(np.abs(d_transform)))
-			tauh = calculate_baseline(pair)/2.9979e8*1e9 # convert to ns
-			ax = plt.gca()
-			ax.axvline(x=0., linestyle='--', color='0.5')
-			ax.axvline(x=-tauh, linestyle='--', color='0.5')
-			ax.axvline(x=tauh, linestyle='--', color='0.5')
-			ax.set_xlim(-400, 400)
-			ax.set_ylim(-10, -2)
-			ax.set_xlabel('Delay [bins]')
-			ax.set_ylabel('log10(abs(V_I)')
-			ax.set_title('Delay Transform'+antstr+stokes)
-			plt.savefig("/data4/paper/rkb/delaygifstorage/"+'delaytransform'+'{} {}.png'.format(antstr, stokes))
-			plt.clf()
+	for antstr in baselines:
+		ant_i, ant_j = map(int, antstr.split('_'))
+		pair = (ant_i, ant_j)
+		data, channels = avgfreqcalc(data_dir, antstr, stokes)
+		window = aipy.dsp.gen_window(channels, window="blackman-harris")
+		d_transform = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(data * window)))
+		delays = np.fft.fftshift(np.fft.fftfreq(channels, .1/channels)) # fftfreq takes in (nchan, chan_spacing)
+		d_start = delays[0]
+		d_end = delays[-1]
+		#d_transform = np.abs(d_transform)
+		f, ax = plt.subplots(figsize=(5, 4))
+		ax.plot(delays, np.log(np.abs(d_transform)))
+		tauh = calculate_baseline(pair)/2.9979e8*1e9 # convert to ns
+		ax = plt.gca()
+		ax.axvline(x=0., linestyle='--', color='0.5')
+		ax.axvline(x=-tauh, linestyle='--', color='0.5')
+		ax.axvline(x=tauh, linestyle='--', color='0.5')
+		ax.set_xlim(-400, 400)
+		ax.set_ylim(-10, -2)
+		ax.set_xlabel('Delay [bins]')
+		ax.set_ylabel('log10(abs(V_I)')
+		ax.set_title('Delay Transform'+antstr+stokes)
+		plt.savefig("/data4/paper/rkb/delaygifstorage/"+'delaytransform'+'{} {}.png'.format(antstr, stokes))
+		plt.clf()
 	images = glob.glob('/data4/paper/rkb/delaygifstorage/*.png')
 	gif = []
 	for filename in images:
@@ -197,24 +167,12 @@ def delaytransformv1(data_dir, stokes):
 def delaytransformavgbaseline(data_dir, stokes):
 	baselines = ['64_88', '64_80', '9_105', '9_53', '53_104', '22_72', '20_22', '20_31', '31_96', '65_89', '10_97', '10_43', '72_105', '88_105', '22_112', '9_22', '9_64', '20_53', '53_80', '10_89', '31_89', '31_104', '43_65', '65_96', '72_112', '97_112', '22_105', '9_88', '9_20', '20_89', '43_89', '53_64', '31_53', '31_65', '80_104', '96_104']
 	avg = 0
-	if stokes == "I" or stokes == "Q":
-		t_xx, d_xx, f_xx = capo.miriad.read_files([xx_data[i]], antstr=antstr, polstr='xx', verbose=True)
-		t_yy, d_yy, f_yy = capo.miriad.read_files([yy_data[i]], antstr=antstr, polstr='yy', verbose=True)
-		for antstr in baselines:
-			ant_i, ant_j = map(int, antstr.split('_'))
-			d_transform = np.fft.ifft(avgfreqcalc(data_dir, antstr, stokes, d_xx, d_yy))
-			d_transform = (np.fft.fftshift(d_transform))
-			d_transform = np.abs(d_transform)
-			avg += d_transform
-	if stokes =="U" or stokes =="V":
-		t_xy, d_xy, f_xy = capo.miriad.read_files([xy_data[i]], antstr=antstr, polstr='xy', verbose=True)
-		t_yx, d_yx, f_yx = capo.miriad.read_files([yx_data[i]], antstr=antstr, polstr='yx', verbose=True)
-		for antstr in baselines:
-				ant_i, ant_j = map(int, antstr.split('_'))
-				d_transform = np.fft.ifft(avgfreqcalc(data_dir, antstr, stokes, d_xy, d_yx))
-				d_transform = (np.fft.fftshift(d_transform))
-				d_transform = np.abs(d_transform)
-				avg += d_transform
+	for antstr in baselines:
+		ant_i, ant_j = map(int, antstr.split('_'))
+		d_transform = np.fft.ifft(avgfreqcalc(data_dir, antstr, stokes))
+		d_transform = (np.fft.fftshift(d_transform))
+		d_transform = np.abs(d_transform)
+		avg += d_transform
 	avg = avg/len(baselines)
 	plt.xlim(400,600)
 	plt.xlabel('Delay [bins]')
--- VIQUVaveraged_over_time.py
+++ VIQUVaveraged_over_time.py
@@ -15,7 +15,10 @@ def avgfreqcalc(data_dir, antstr, stokes, d_xx, d_yy):
 	avg_freq = None
 	n_avg = 0
 	# loop over files
+
 	if stokes == "I" or stokes == "Q":
+		t_xx, d_xx, f_xx = capo.miriad.read_files([xx_data[i]], antstr=antstr, polstr='xx', verbose=True)
+		t_yy, d_yy, f_yy = capo.miriad.read_files([yy_data[i]], antstr=antstr, polstr='yy', verbose=True)
 		for i in np.arange(len(xx_data)):
 			vis_xx = d_xx[(ant_i, ant_j)]['xx']
 			vis_yy = d_yy[(ant_i, ant_j)]['yy']
@@ -33,6 +36,9 @@ def avgfreqcalc(data_dir, antstr, stokes, d_xx, d_yy):
 					avg_freq += np.abs(stokes_Q[it, :])
 					n_avg += 1
 	elif stokes == "V" or stokes == "U":
+		t_xy, d_xy, f_xy = capo.miriad.read_files([xy_data[i]], antstr=antstr, polstr='xy', verbose=True)
+		t_yx, d_yx, f_yx = capo.miriad.read_files([yx_data[i]], antstr=antstr, polstr='yx', verbose=True)
+
 		for i in np.arange(len(xy_data)):
 			vis_xy = d_xy[(ant_i, ant_j)]['xy']
 			vis_yx = d_yx[(ant_i, ant_j)]['yx']
