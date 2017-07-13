5bd7f6cd67c6a0b883bc2ec0ba355798da111cd7
Roshan Benefo <rbenefoz@gmail.com>
2017-07-13 00:44:55 -0400

    bug crushing

--- VIQUVaveraged_over_time.py
+++ VIQUVaveraged_over_time.py
@@ -37,7 +37,7 @@ def avgfreqcalc(data_dir, antstr, stokes):
 				for it in range(vis_xx.shape[0]):    
 					avg_freq += np.abs(stokes_Q[it, :])
 					n_avg += 1
-	elif stokes == "U" or "V":
+	if stokes == "U" or "V":
 		for i in np.arange(len(xy_data)):
 			t_xy, d_xy, f_xy = capo.miriad.read_files([xy_data[i]], antstr=antstr, polstr='xy', verbose=True)
 			t_yx, d_yx, f_yx = capo.miriad.read_files([yx_data[i]], antstr=antstr, polstr='yx', verbose=True)
