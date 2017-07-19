042d8cbc2151006e2c5329b5ca55237798c9f1e0
Roshan Benefo <rbenefoz@gmail.com>
2017-07-19 09:25:22 -0400

    test

--- VIQUVaveraged_over_time.py
+++ VIQUVaveraged_over_time.py
@@ -26,9 +26,9 @@ def avgfreqall(data_dir):
 			ant_j = x[elem][1]
 			slope = x[elem][2]
 			if len(antstr_all) <= 5:
-				antstr_all += "%s_%s"+"," % (x[elem][0], x[elem][1])
+				antstr_all += "{}_{}".format (x[elem][0], x[elem][1]) + ","
 			else:
-				antstr_all += "," + "%s_%s" % (x[elem][0], x[elem][1])
+				antstr_all += "," + "{}_{}".format(x[elem][0], x[elem][1])
 			
 			antstr_all = antstr_all[:-1]
 			print (antstr_all)
