5a9e6e53b4033bd9a6bbe3b747b974dfd62f2bf2
Roshan Benefo <rbenefoz@gmail.com>
2017-07-20 11:23:08 -0400

    Test

--- uvfitsreader.py
+++ uvfitsreader.py
@@ -11,8 +11,10 @@ def uvreader(data_dir):
 	for uvfits_file in datafiles:
 		UV.read_uvfits(uvfits_file)
 		data = UV.get_data(53, 97)
-		print(data.shape)
-		data2 = UV.get_data(UV.antnums_to_baseline(53,97))
-		print(np.all(data == data2))
+		xx_data = data[::,1]
+		print(xx_data.shape)
+		# print(data.shape)
+		# data2 = UV.get_data(UV.antnums_to_baseline(53,97))
+		# print(np.all(data == data2))
 		# plt.imshow(np.abs(data, data2))
 		# plt.savefig("/data4/paper/rkb/"+ "uvreadertest.png")
