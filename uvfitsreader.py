dbade8b29033271d100f3c92574336c0dcabd76f
Roshan Benefo <rbenefoz@gmail.com>
2017-07-20 11:07:45 -0400

    change antennapair"

--- uvfitsreader.py
+++ uvfitsreader.py
@@ -7,8 +7,8 @@ def uvreader(data_dir):
 	datafiles = sorted(glob.glob(''.join([data_dir, 'zen.*.HH.uvc.vis.uvfits'])))
 	for uvfits_file in datafiles:
 		UV.read_uvfits(uvfits_file)
-		data = UV.get_data(97, 112)
+		data = UV.get_data(53, 97)
 		print(data.shape)
-		data2 = UV.get_data(UV.antnums_to_baseline(97,112))
+		data2 = UV.get_data(UV.antnums_to_baseline(53,97))
 		print(np.all(data == data2))
 		plt.imshow(np.abs(data, data2))
