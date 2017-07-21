be5e0aef54c93b51b0f1e5305ac5a294cb4569b9
Roshan Benefo <rbenefoz@gmail.com>
2017-07-21 10:02:37 -0400

    test

--- uvfitsreader.py
+++ uvfitsreader.py
@@ -48,12 +48,13 @@ def uvreader3(data_dir):
 def uvreader5(data_dir):
 	datafiles = sorted(glob.glob(''.join([data_dir, 'zen.*.HH.uvc.vis.uvfits'])))
 	total_array = np.empty([56, 1024, 4])
-	allpairs = UV.get_antpairs()
-	for uvfits_file in datafiles:
-		UV.read_uvfits(uvfits_file)
-		for baseline in allpairs:
-			data = UV.get_data(baseline)
-			print (data)
+	#allpairs = UV.get_antpairs()
+	print (UV.get_antpairs())
+	# for uvfits_file in datafiles:
+	# 	UV.read_uvfits(uvfits_file)
+	# 	for baseline in allpairs:
+	# 		data = UV.get_data(baseline)
+	# 		print (data)
 		#np.concatenate((total_array, data), axis=0)
 	#print (total_array)
 	#np.save("/data4/paper/rkb/zenuvfitssave.vis.uvfits", total_array)
