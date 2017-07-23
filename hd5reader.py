2ede8cbb4eff9053a2b9f995fa9d4a8ffcac33d4
Roshan Benefo <rbenefoz@gmail.com>
2017-07-23 00:40:28 -0400

    printtest

--- hd5reader.py
+++ hd5reader.py
@@ -1,10 +1,30 @@
 import h5py
+import matplotlib
+matplotlib.use('Agg')
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 fn = '/home/plaplant/global_signal/Output/HERA/beam_zenith/xi_nu_phi.hdf5'
 f = h5py.File(fn, 'r')
 
-for key in f.keys():
-	print(key)
-dgrp = f["/Data"]
-for key in dgrp.keys():
-	print(key)
+
+dset_nu = dgrp["nu"]
+nu = np.asarray(dset_nu)
+dset_xi = dgrp("xi")
+
+xi_stokes = xi[:2]
+print (xi_stokes)
+# for baselin_sep in xi:
+
+# xi = np.asarray(dset_xi)
+# plt.plot(nu, np.abs(xi0))
+
+# plt.legend()
+# plt.savefig("xi.pdf")
+
+# for key in f.keys():
+# 	print(key)
+# dgrp = f["/Data"]
+# for key in dgrp.keys():
+# 	print(key)
