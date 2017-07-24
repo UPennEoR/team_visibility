7927b91b5ee28f128a5fbb4793e7103af65aa29a
Roshan Benefo <rbenefoz@gmail.com>
2017-07-24 18:12:38 -0400

    fix bugs

--- uvfitsreader.py
+++ uvfitsreader.py
@@ -73,6 +73,7 @@ def uvreader5(data_dir):
 	for uvfits_file in datafiles:
 		UV.read_uvfits(uvfits_file)
 		for baseline in antpairall:
+			baseline = baseline.strip("()")
 			xx_data = UV.get_data(baseline, 'xx')
 			print (xx_data)
 			data = UV.get_data(baseline)
