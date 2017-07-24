f67abc0863e9e6ddb1584e47b0c71aa67cc0cb21
Roshan Benefo <rbenefoz@gmail.com>
2017-07-24 18:16:37 -0400

    test

--- uvfitsreader.py
+++ uvfitsreader.py
@@ -73,7 +73,7 @@ def uvreader5(data_dir):
 	for uvfits_file in datafiles:
 		UV.read_uvfits(uvfits_file)
 		for baseline in antpairall:
-			baseline = ' '.join(map(baseline))
+			baseline = ','.join(map(str, baseline))
 			xx_data = UV.get_data(baseline, 'xx')
 			print (xx_data)
 			data = UV.get_data(baseline)
