9fc50417240047b4b3e3c5893a6eca72cf7b24a9
Roshan Benefo <rbenefoz@gmail.com>
2017-07-24 10:44:40 -0400

    fix bugs

--- hd5reader.py
+++ hd5reader.py
@@ -31,7 +31,7 @@ def stokescreator(stokes):
 	plt.legend()
 	plt.title('stokes {}'.format(stokes))
 	plt.xlabel('Frequency (MHz)')
-	plt.ylabel('Xi')
+	plt.ylabel('Avg Power')
 	plt.savefig('/data4/paper/rkb/hd5saves/hd5test1.png')
 
 def baselinetest(fn):
@@ -46,14 +46,15 @@ def baselinetest(fn):
 	dset_xi = dgrp["xi"]
 	xi = np.asarray(dset_xi)
 	xi_baseline = xi[:, 0, 0, 0]
-	y = []
 	for index, i in enumerate(np.nditer(xi_baseline)):
 		xi_plot = xi[index, 0, 0, 0, : ]
-		plt.plot(nu, np.abs(xi_stokes), linestyle='-', label="{}".format(i))
+		plt.plot(nu, np.abs(xi_stokes), linestyle='-', label="{}".format(index))
 	plt.legend()
 	plt.xlabel('Frequency (MHz)')
-	plt.ylabel('Xi')
+	plt.ylabel('Avg Power')
 	plt.savefig('/data4/paper/rkb/hd5saves/hd5test2.png')
+
+	# compare sam's plots against .vis.uvfits file data!
 
 
 
