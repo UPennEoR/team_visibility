04e3a496b7d38f66fec3320c9a90afb7a7b77a34
Roshan Benefo <rbenefoz@gmail.com>
2017-07-23 22:56:09 -0400

    add plt creation

--- hd5reader.py
+++ hd5reader.py
@@ -4,18 +4,25 @@ matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import glob
 
+
+if os.path.isdir("/data4/paper/rkb/hd5saves/"):
+	pass
+else:
+	os.makedirs("/data4/paper/rkb/hd5saves/")
 fn = '/home/plaplant/global_signal/Output/HERA/beam_zenith/xi_nu_phi.hdf5'
 f = h5py.File(fn, 'r')
-
 dgrp = f["/Data"]
 dset_nu = dgrp["nu"]
 nu = np.asarray(dset_nu)
 dset_xi = dgrp["xi"]
 xi = np.asarray(dset_xi)
-
-xi_stokes = xi[0, :, 0, 0]
-print (xi_stokes)
+xi_stokesI = xi[0, 1, 0, :]
+plt.plot(nu, xi_stokesI.real, color='b', linestyle='-', label="I real")
+plt.plot(nu, xi_stokesI.imag, color='b', linestyle='-', label="I real")
+plt.legend()
+plt.savefig('/data4/paper/rkb/hd5saves/hd5test1.png')
 
 # for baselin_sep in xi:
 
