945472ba3af0ef4ca7c1c7bae29054738cd07617
Roshan Benefo <rbenefoz@gmail.com>
2017-07-24 09:51:35 -0400

    add baselinetest

--- hd5reader.py
+++ hd5reader.py
@@ -6,23 +6,43 @@ import matplotlib.pyplot as plt
 import os
 import glob
 
+def stokescreator(stokes):
+	if os.path.isdir("/data4/paper/rkb/hd5saves/"):
+		pass
+	else:
+		os.makedirs("/data4/paper/rkb/hd5saves/")
+	fn = '/home/plaplant/global_signal/Output/HERA/beam_zenith/xi_nu_phi.hdf5'
+	f = h5py.File(fn, 'r')
+	dgrp = f["/Data"]
+	dset_nu = dgrp["nu"]
+	nu = np.asarray(dset_nu)
+	dset_xi = dgrp["xi"]
+	xi = np.asarray(dset_xi)
+	if stokes == "I":
+		xi_stokes = xi[0, 0, 0, :]
+	elif stokes == "Q":
+		xi_stokes = xi[0, 1, 0, :]
+	elif stokes == "U":
+		xi_stokes = xi[0, 2, 0, :]
+	elif stokes == "V":
+		xi_stokes = xi[0, 3, 0, :]
+	plt.plot(nu, np.abs(xi_stokes), color='b', linestyle='-', label="Absolute value")
+	plt.legend()
+	plt.title('stokes {}'.format(stokes))
+	plt.xlabel('Frequency (MHz)')
+	plt.ylabel('Xi')
+	plt.savefig('/data4/paper/rkb/hd5saves/hd5test1.png')
 
-if os.path.isdir("/data4/paper/rkb/hd5saves/"):
-	pass
-else:
-	os.makedirs("/data4/paper/rkb/hd5saves/")
-fn = '/home/plaplant/global_signal/Output/HERA/beam_zenith/xi_nu_phi.hdf5'
-f = h5py.File(fn, 'r')
-dgrp = f["/Data"]
-dset_nu = dgrp["nu"]
-nu = np.asarray(dset_nu)
-dset_xi = dgrp["xi"]
-xi = np.asarray(dset_xi)
-xi_stokesI = xi[0, 1, 0, :]
-plt.plot(nu, xi_stokesI.real, color='b', linestyle='-', label="I real")
-plt.plot(nu, xi_stokesI.imag, color='b', linestyle='-', label="I imag")
-plt.legend()
-plt.savefig('/data4/paper/rkb/hd5saves/hd5test1.png')
+def baselinetest(fn):
+	f = h5py.File(fn, 'r')
+	dgrp = f["/Data"]
+	dset_nu = dgrp["nu"]
+	nu = np.asarray(dset_nu)
+	dset_xi = dgrp["xi"]
+	xi = np.asarray(dset_xi)
+	xi_baseline = xi[:, 0, 0, 0]
+	print (xi_baseline)
+
 
 # for baselin_sep in xi:
 
