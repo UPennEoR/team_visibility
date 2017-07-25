991cbb9e7c149c3b10e455d0a36d2a142b06cfd9
Roshan Benefo <rbenefoz@gmail.com>
2017-07-25 09:59:24 -0400

    test

--- uvfitsreader.py
+++ uvfitsreader.py
@@ -85,9 +85,10 @@ def uvreader5(data_dir):
 				avg += i
 			n_avg = avg/len(np.nditer(averager))
 			t_avg = t_avg.append(n_avg)
-		plt.plot(t_avg)
-		plt.savefig("/data4/paper/rkb/uvreaderstorage/testgraph{}.png".format(baseline))
-		plt.clf()
+			print(t_avg)
+		# plt.plot(t_avg)
+		# plt.savefig("/data4/paper/rkb/uvreaderstorage/testgraph{}.png".format(baseline))
+		# plt.clf()
 			# plt.imshow((np.log10(np.abs(vis_xx))), aspect='auto',
 			# 		   vmax=0, vmin=-6, cmap='viridis')
 			# plt.xlabel('frequency')
