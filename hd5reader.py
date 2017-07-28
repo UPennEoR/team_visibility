68a0c9e9722131c8256a41320e81f5eb35b3f41e
Roshan Benefo <rbenefoz@gmail.com>
2017-07-28 11:04:55 -0400

    Fix bugs

--- hd5reader.py
+++ hd5reader.py
@@ -17,16 +17,20 @@ def layoftheland(data_dir):
  		print(key)
  	dset_nu = dgrp["nu"]
  	nu = np.asarray(dset_nu)
- 	print ("Nu shape:" nu.shape)
+ 	print ("Nu shape:")
+ 	print(nu.shape)
  	dset_phi = dgrp["phi"]
  	phi = np.asarray(dset_phi)
- 	print ("Phi shape:" phi.shape)
+ 	print ("Phi shape:")
+ 	print(phi.shape)
  	dset_tauh = dgrp["tauh"]
  	tauh = np.asarray(dset_tauh)
- 	print ("Tauh shape:" tauh.shape)
+ 	print ("Tauh shape:")
+ 	print(tauh.shape)
  	dset_xi = dgrp["xi"]
  	xi = np.asarray(dset_xi)
- 	print ("Xi shape:" xi.shape)
+ 	print ("Xi shape:")
+ 	print(xi.shape)
 
 
 def stokescreator(stokes):
