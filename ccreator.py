dd5dc492287fe4973f98727b66a2aea88e17358a
Roshan Benefo <rbenefoz@gmail.com>
2017-10-30 22:42:43 -0400

    finish ccreator

--- ccreator.py
+++ ccreator.py
@@ -3,11 +3,19 @@ import numpy as np
 import glob
 UV = UVData()
 
+#**conjugate zac's data
+#instead of stacking them and then collapsing, add as you go. Zach's data isn't aligned 
+#with the actual data.
+
+files = [f for f in os.listdir(visibility_dir) if f[-4:] == '.uvc']
+uv = UVData()
+uv.read_miriad(files)
+
 def ccreator(data_dir):
-	# zacfiles = sdkfjsdhlksjhf
+	zacxxdatafiles = sorted(glob.glob(''.join(['/data4/paper/HERA2015/2457458/', 'zen.*.xx.HH.uvc'])))
 	xxdatafiles = sorted(glob.glob(''.join([data_dir, 'zen.*.xx.HH.uvc'])))
 	baselines = [(9, 20), (20, 89), (53, 31)]
-	xxdatalist2 = np.empty((4032, 1024, 28), dtype=np.complex128)
+	xxdatalist2 = np.empty((4032, 1024, 3), dtype=np.complex128)
 	UV.read_miriad(xxdatafiles[0])
 	antpairall= UV.get_antpairs()
 	for miriad_file in xxdatafiles:
@@ -16,30 +24,31 @@ def ccreator(data_dir):
 		i = 0
 		for baseline in baselines:
 			xxrealdata = UV.get_data(baseline)
-			if xxrealdata.shape != (56, 1024):
-				pass
-			else:
-				xxdatalist = np.dstack((xxdatalist, xxrealdata))
-				i += 1
+			xxdatalist = np.dstack((xxdatalist, xxrealdata))
+			i += 1
 		print(xxdatalist.shape)
-		if xxdatalist.shape != (56, 1024, 28):
-			pass
-		else:
-			xxdatalist2 = np.hstack(xxdatalist)
+		xxdatalist2 += xxdatalist
 		print(xxdatalist2.shape)
-
-	# c = []
-	# f = open("/data4/paper/rkb/Cvals.txt", "w")
-	# for i in len(baselines):
-	# 	mult = xxdatalist2[:, :, i] * zacxxdatalist2[:, :, i]
-	# 	square = xxdatalist2[:, :, i] *xxdatalist2[:, :, i]
-	# 	#now, sum it all by collapsing in time, and frequency
-	# 	xxmulttimetotal = np.sum(mult, axis = 0)
-	# 	xxmultcompletetotal = np.sum(xxtimetotal, axis = 0)
-	# 	xxsquaretimetotal = np.sum(square, axis = 0)
-	# 	xxsquarecompletetotal=np.sum(xxsquaretimetotal, axis = 0)
-	# 	#Divide
-	# 	c.append(xxmultcompletetotal/xxsquarecompletetotal)
-	# 	f.write("%s, %s" c[i], baseline[i])
-	# f.close()
+	zacxxdatalist2 = np.empty((4032, 1024, 3), dtype=np.complex128)
+	for file in zacxxdatafiles:
+		UV.read_miriad(file)
+		zacxxdatalist = np.empty((56, 1024))
+		for baseline in baselines:
+			xxzacdata = UV.get_data(baseline)
+			zacxxdatalist = np.dstack((zacxxdatalist, xxzacdata))
+		zacxxdatalist2 += zacxxdatalist
+	c = []
+	f = open("/data4/paper/rkb/Cvals.txt", "w")
+	for i in len(baselines):
+		mult = xxdatalist2[:, :, i] * zacxxdatalist2[:, :, i]
+		square = xxdatalist2[:, :, i] *xxdatalist2[:, :, i]
+		#now, sum it all by collapsing in time, and frequency
+		xxmulttimetotal = np.sum(mult, axis = 0)
+		xxmultcompletetotal = np.sum(xxtimetotal, axis = 0)
+		xxsquaretimetotal = np.sum(square, axis = 0)
+		xxsquarecompletetotal=np.sum(xxsquaretimetotal, axis = 0)
+		#Divide
+		c.append(xxmultcompletetotal/xxsquarecompletetotal)
+		f.write("%s, %s \n" c[i], baseline[i])
+	f.close()
 
