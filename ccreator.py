f8d6bab8892b1a433b5ba65f64eafdb929328a55
Roshan Benefo <rbenefoz@gmail.com>
2017-11-02 16:50:44 -0400

    add adjuster

--- ccreator.py
+++ ccreator.py
@@ -2,7 +2,14 @@ from pyuvdata import UVData
 import numpy as np
 import glob
 UV = UVData()
-
+#CONVERT ZAC'S FILES TO LST. COMBINE ALL OF ZAC'S DATA, AND SPLIT INTO 1 FILE EVERY LST HOUR OR SO
+#UV.Nbls
+#UV.Ntimes
+#Maybe use UV.set_lsts_from_time_array() ??
+#UV.get_times(baseline)
+#LST = UV.lst_array
+#LST_1b1 = LST(np.arange(0, UV.Nbls*UV.Ntimes, UV.Nbls))
+#convert to degrees: np.degrees(LST_1b1)/15
 #**conjugate zac's data
 #instead of stacking them and then collapsing, add as you go. Zach's data isn't aligned 
 #with the actual data.
@@ -10,6 +17,30 @@ UV = UVData()
 # files = [f for f in os.listdir(visibility_dir) if f[-4:] == '.uvc']
 # uv = UVData()
 # uv.read_miriad(files)
+#you're not actually adding via time yet. Do this. You're adding all the files together, but you need to also add the times together
+
+#develop LST calc
+#add conjugate to zac's data
+#**MUST FIX: MUST MULTIPLY BEFORE SUMMINNGGGGG!!!
+
+def zacadjuster(data_dir):
+	zacxxdatafiles = sorted(glob.glob(''.join([data_dir, 'zen.*.xx.HH.uvc'])))
+	antpairfile = zacxxatafiles[0]
+	simxxdatalist = np.empty((0, 1024, 1), dtype=np.complex128) #initialize biglist
+	baselines = [(9, 20)]
+	for miriad_file in zacxxdatafiles:
+		UV.read_miriad(miriad_file)
+		xxsimdata = UV.get_data(baseline)
+		#concatenate by time
+		biglist = np.concatenate(biglist, xxsimdata, axis= 0)
+	LST = UV.lst_array
+	LST_1b1 = LST(np.arange(0, UV.Nbls*UV.Ntimes, UV.Nbls))
+	indegrees = np.degrees(LST_1b1)/15
+	print(LST_1b1)
+
+
+
+
 
 def ccreator(data_dir):
 	zacxxdatafiles = sorted(glob.glob(''.join(['/data4/paper/HERA2015/2457458/', 'zen.*.xx.HH.uvc'])))
@@ -18,12 +49,16 @@ def ccreator(data_dir):
 	xxdatalist2 = np.empty((56, 1024, 3), dtype=np.complex128)
 	UV.read_miriad(xxdatafiles[0])
 	antpairall= UV.get_antpairs()
+	LST_1b1 = LST(np.arange(0, UV.Nbls*UV.Ntimes, UV.Nbls))
+	np.degrees(LST_1b1)/15
 	for miriad_file in xxdatafiles:
 		UV.read_miriad(miriad_file)
 		xxdatalist = np.empty((56, 1024), dtype=np.complex128)
 		i = 0
 		for baseline in baselines:
 			xxrealdata = UV.get_data(baseline)
+			UV.get_times(baseline)
+
 			if i == 0:
 				xxdatalist += xxrealdata
 			else:
@@ -31,6 +66,7 @@ def ccreator(data_dir):
 			print(i)
 			i += 1
 		print(xxdatalist.shape)
+		xxdatalist = np.concatenate(xxdatalist, axis = 0)
 		xxdatalist2 += xxdatalist
 		print(xxdatalist2.shape)
 	zacxxdatalist2 = np.empty((56, 1024, 3), dtype=np.complex128)
