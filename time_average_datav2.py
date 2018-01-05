2e4d474246895b69c6f0c2f00b75c42dc9806899
Roshan Benefo <rbenefoz@gmail.com>
2018-01-05 11:29:36 -0500

    add baselines

--- time_average_datav2.py
+++ time_average_datav2.py
@@ -18,11 +18,9 @@ Ntimes = 4133
 
 # define all east-west 14 meter baselines
 ew14m = [
-    '9_64',
-    '10_89',
-    '31_104',
-    '53_80',
-    '112_22',
+    '9_53',
+    '20_31',
+    '22_20',
 ]
 
 def time_average_data(data_dir, outfile, file_type):
